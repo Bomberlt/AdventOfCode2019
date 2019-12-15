54cc151838f2e5422eb60281c33fcf1abcb679c2
Povilas Galcius <pofeliuxxx@gmail.com>
2019-12-15 15:51:24 +0200

    Day3 - Move Left function

--- src/day3.js
+++ src/day3.js
@@ -3,7 +3,8 @@ export function move(direction, moves, startPos) {
   const startY = startPos[1];
   let marked = [];
   for (let i = 0; i < moves; i++) {
-    const pos = [startX + 1 + i, startY];
+    const pos =
+      direction == 'R' ? [startX + 1 + i, startY] : [startX - 1 - i, startY];
     marked.push({ pos, marked: true });
   }
   return marked;
--- src/day3.test.js
+++ src/day3.test.js
@@ -2,38 +2,108 @@ import { move } from './day3';
 
 describe('day2', () => {
   //op marks to right direction
+  //move marks right amount of steps
   describe('move', () => {
     describe('from 0,0', () => {
-      it('right 1 marks 1 cells to right', () => {
-        const result = move('R', 1, [0, 0]);
+      describe('right', () => {
+        it('1 marks 1 cells to right', () => {
+          const result = move('R', 1, [0, 0]);
 
-        expect(result).toContainEqual({ pos: [1, 0], marked: true });
+          expect(result).toContainEqual({
+            pos: [1, 0],
+            marked: true
+          });
+        });
+
+        it('2 marks 2 cells to right', () => {
+          const result = move('R', 2, [0, 0]);
+
+          expect(result).toContainEqual({
+            pos: [1, 0],
+            marked: true
+          });
+          expect(result).toContainEqual({
+            pos: [2, 0],
+            marked: true
+          });
+        });
       });
 
-      it('right 2 marks 2 cells to right', () => {
-        const result = move('R', 2, [0, 0]);
+      describe('left', () => {
+        it('1 marks 1 cells to left', () => {
+          const result = move('L', 1, [0, 0]);
 
-        expect(result).toContainEqual({ pos: [1, 0], marked: true });
-        expect(result).toContainEqual({ pos: [2, 0], marked: true });
+          expect(result).toContainEqual({
+            pos: [-1, 0],
+            marked: true
+          });
+        });
+
+        it('2 marks 2 cells to left', () => {
+          const result = move('L', 2, [0, 0]);
+
+          expect(result).toContainEqual({
+            pos: [-1, 0],
+            marked: true
+          });
+          expect(result).toContainEqual({
+            pos: [-2, 0],
+            marked: true
+          });
+        });
       });
     });
     describe('from 1,1', () => {
-      it('right 1 marks 1 cells to right', () => {
-        const result = move('R', 1, [1, 1]);
+      describe('right', () => {
+        it('1 marks 1 cells to right', () => {
+          const result = move('R', 1, [1, 1]);
 
-        expect(result).toContainEqual({ pos: [2, 1], marked: true });
+          expect(result).toContainEqual({
+            pos: [2, 1],
+            marked: true
+          });
+        });
+
+        it('2 marks 2 cells to right', () => {
+          const result = move('R', 2, [1, 1]);
+
+          expect(result).toContainEqual({
+            pos: [2, 1],
+            marked: true
+          });
+          expect(result).toContainEqual({
+            pos: [3, 1],
+            marked: true
+          });
+        });
       });
 
-      it('right 2 marks 2 cells to right', () => {
-        const result = move('R', 2, [1, 1]);
+      describe('left', () => {
+        it('1 marks 1 cells to left', () => {
+          const result = move('L', 1, [1, 1]);
 
-        expect(result).toContainEqual({ pos: [2, 1], marked: true });
-        expect(result).toContainEqual({ pos: [3, 1], marked: true });
+          expect(result).toContainEqual({
+            pos: [0, 1],
+            marked: true
+          });
+        });
+
+        it('2 marks 2 cells to left', () => {
+          const result = move('L', 2, [1, 1]);
+
+          expect(result).toContainEqual({
+            pos: [0, 1],
+            marked: true
+          });
+          expect(result).toContainEqual({
+            pos: [-1, 1],
+            marked: true
+          });
+        });
       });
     });
   });
 
-  //move marks right amount of steps
   //move marks intersection and returns intersection data
   //array of commands executes moves
   //distance returns distance from central point
